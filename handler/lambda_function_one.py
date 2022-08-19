d1d40cb4f562c2ddf6a56ecb1282428182da3f49
AnmolQuantel <anmol.dubey@quantel.ai>
2022-08-19 22:23:22 +0530

    func 6 added

--- handler/lambda_function_one.py
+++ handler/lambda_function_one.py
@@ -4,4 +4,4 @@ import re
 
 
 def lambda_handler(event, context):
-    print('hello this is Anmol making change in function 1, now made some more changes here')
+    print('hello this is Anmol making change in function 1, now made some more changes here, some change here as well')
